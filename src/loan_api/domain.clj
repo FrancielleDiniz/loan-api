aa1c03dcbaec87bf24ae09ab0ffd02352f021f9c
Francielle Queiroz <fdiniz.queiroz@gmail.com>
2026-02-17 19:13:14 -0300

    Add domain input validation with structured errors

--- src/loan_api/domain.clj
+++ src/loan_api/domain.clj
@@ -1,8 +1,42 @@
 (ns loan-api.domain)
 
+(defn- valid-number? [n]
+  (and (number? n) (not (neg? n))))
+
+(defn- valid-months? [m]
+  (and (integer? m) (pos? m)))
+
+(defn- validate-input!
+  [{:keys [amount rate months] :as input}]
+  (cond
+    (nil? amount)
+    (throw (ex-info "Amount is required" {:type :validation-error :field :amount}))
+
+    (nil? rate)
+    (throw (ex-info "Rate is required" {:type :validation-error :field :rate}))
+
+    (nil? months)
+    (throw (ex-info "Months is required" {:type :validation-error :field :months}))
+
+    (not (valid-number? amount))
+    (throw (ex-info "Amount must be a positive number"
+                    {:type :validation-error :field :amount}))
+
+    (not (valid-number? rate))
+    (throw (ex-info "Rate must be a non-negative number"
+                    {:type :validation-error :field :rate}))
+
+    (not (valid-months? months))
+    (throw (ex-info "Months must be a positive integer"
+                    {:type :validation-error :field :months}))
+
+    :else true))
+
 (defn calculate-loan
-  [{:keys [amount rate months]}]
-  (let [total (* amount (Math/pow (+ 1 rate) months))
+  [input]
+  (validate-input! input)
+  (let [{:keys [amount rate months]} input
+        total (* amount (Math/pow (+ 1 rate) months))
         installment (/ total months)]
     {:total (double total)
      :installment (double installment)}))
--- test/loan_api/domain_test.clj
+++ test/loan_api/domain_test.clj
@@ -18,3 +18,17 @@
       (is (> (:total result) 10000))
       (is (= (/ (:total result) 12)
              (:installment result))))))
+
+(deftest calculate-loan-invalid-input-test
+  (testing "throws validation error for invalid amount"
+    (is (thrown-with-msg?
+         clojure.lang.ExceptionInfo
+         #"Amount must be a positive number"
+         (domain/calculate-loan {:amount -10 :rate 0.02 :months 12}))))
+
+  (testing "throws validation error for invalid months"
+    (is (thrown-with-msg?
+         clojure.lang.ExceptionInfo
+         #"Months must be a positive integer"
+         (domain/calculate-loan {:amount 1000 :rate 0.02 :months 0})))))
+
